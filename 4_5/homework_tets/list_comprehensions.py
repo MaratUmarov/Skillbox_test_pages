c7a67e2de07e52792571be2988b6fe9d2dc30d24
Marat Umarov <mar982@mail.ru>
2024-03-27 19:59:58 +0300

    work on split and join methods

--- 4_5/homework_tets/list_comprehensions.py
+++ 4_5/homework_tets/list_comprehensions.py
@@ -1,4 +1,4 @@
-
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
-flattened_list = [i_item for secondary_sub in [item for sublist in nice_list for item in sublist] for i_item in secondary_sub]
+flattened_list = [i_item for secondary_sub in [item for sublist in nice_list for item in sublist] for i_item in
+                  secondary_sub]
 print(flattened_list)
